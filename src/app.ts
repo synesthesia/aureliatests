06dcb7e66328e87dabc91e6fef67de22729b422f
Julian Elve <julian.elve@gmail.com>
2017-09-22 14:01:13 +0100

    basic stnadard router

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,35 @@
+import { Aurelia, PLATFORM } from 'aurelia-framework';
+import { Router, RouterConfiguration } from 'aurelia-router';
+
 export class App {
-  message = 'Hello World!';
+  router: Router;
+
+  message = 'Hello World!!';
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    
+    config.title = 'Test router';
+    
+    config.map([{
+        route: ['', 'home'],
+        name: 'home',
+        settings: { icon: 'home' },
+        moduleId: PLATFORM.moduleName('./components/home/home'),
+        nav: true,
+        title: 'Home'
+    }, 
+    
+    {
+        route: 'counter',
+        name: 'counter',
+        settings: { icon: 'education' },
+        moduleId: PLATFORM.moduleName('./components/counter/counter'),
+        nav: true,
+        title: 'Counter'
+    }
+    
+    ]);
+    this.router = router;
+  }
+    
 }
