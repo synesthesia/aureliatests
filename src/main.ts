5aaea6e0957cd21b92ff1bc47ecb5970861a8c5a
Julian Elve <julian.elve@gmail.com>
2017-09-25 10:48:49 +0100

    before materializecss and materializecss bridge

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import {Aurelia} from 'aurelia-framework'
+import {Aurelia, PLATFORM} from 'aurelia-framework'
 import environment from './environment';
-
+import { ConfigBuilder } from 'aurelia-materialize-bridge';
+/*
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
@@ -16,3 +17,22 @@ export function configure(aurelia: Aurelia) {
 
   aurelia.start().then(() => aurelia.setRoot());
 }
+*/
+export async function configure(aurelia: Aurelia) {
+  
+      return aurelia.loader.loadModule(PLATFORM.moduleName('materialize-css')).then(() => {
+          aurelia.use.standardConfiguration();
+          //LogManager.addAppender(new ConsoleAppender());
+  
+          if (environment.debug) {
+            aurelia.use.developmentLogging();
+              //LogManager.setLevel(LogManager.logLevel.debug);
+          }
+          
+          aurelia.use.plugin(PLATFORM.moduleName('aurelia-materialize-bridge'), (b: ConfigBuilder) => b.useAll());
+          //log.info('Aurelia home component loading');
+          return aurelia.start().then(() => aurelia.setRoot());
+          
+      });
+         
+  }
